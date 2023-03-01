d87f86fb136cb20ee2b0834e8e438316fb68c6d0
Vladislav <ragozin.vladis@gmail.com>
2023-03-01 12:07:34 +0300

    add custom user

---
+++ core/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from core.models import User
+
+
+@admin.register(User)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("email", "first_name", "last_name", "username")
+    fields = ("username", "first_name", "last_name", "email", "is_staff", "is_active", "date_joined", "last_login")
+    readonly_fields = ("date_joined", "last_login")
